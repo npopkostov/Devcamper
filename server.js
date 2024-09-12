5dee5f26751d6984b12af2cbcbde55c240e850ef
npopkostov <npopkostov@hotmail.com>
2024-09-12 19:11:07 +0200

    MongoDB Update

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootamps");
+
 const app = express();
 
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
+app.use(logger);
 
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
